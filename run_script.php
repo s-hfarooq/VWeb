4d9e560a3d35e67bcddc1f41bb03792e59ae4d10
Hassan Farooq <hassan@pop-os.localdomain>
2019-11-02 10:29:17 -0500

    PHP

--- run_script.php
+++ run_script.php
@@ -1,16 +1,10 @@
 <?php
-ini_set('set_time_limit', 300000);
-	$dir = '/var/www/html/files';
-	if(!file_exists($dir)) {
-		mkdir($dir, 0744);
-	}
-
-  $command = escapeshellcmd('/usr/bin/python3 /var/www/html/test.py');
-//	sleep(10);
-$output = shell_exec($command);
-echo "test";
-  echo $output;
-echo "test2";
-	file_put_contents('/var/www/html/files/output.txt', $output);
- header('Location: http://23.99.220.51/displayUserGraph.html?success=true');
+	echo $_GET["myCountry"];
+	$command = escapeshellcmd('/usr/bin/python3 /var/www/html/test.py');
+	$output = shell_exec($command);
+	echo "test";
+	echo $output;
+	echo "test2";
+	//file_put_contents('/var/www/html/files/output.txt', $output);
+	//header('Location: succ.html?success=true');
 ?>
