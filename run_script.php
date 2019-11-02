df8e78a505928ad702b6341e3762b368805132fb
Hassan Farooq <hassan@pop-os.localdomain>
2019-11-02 14:16:17 -0500

    Updated site

--- run_script.php
+++ run_script.php
@@ -4,6 +4,6 @@
 	$command = escapeshellcmd('/usr/bin/python3 /var/www/html/test.py ' . $inputVal);
 	$output = shell_exec($command);
 	echo $output;
-	//file_put_contents('/var/www/html/files/output.txt', $output);
-	//header('Location: succ.html?success=true');
+	file_put_contents('/var/www/html/files/output.txt', $output);
+	header('Location: results.html?success=true');
 ?>
